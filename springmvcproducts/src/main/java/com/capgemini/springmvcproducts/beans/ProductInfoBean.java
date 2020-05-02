de2c0cb6947ef44d1feca897ffc1d95734643904
prasant2677 <61830386+prasant2677@users.noreply.github.com>
2020-05-02 08:24:02 +0530

    Add files via upload

---
+++ springmvcproducts/src/main/java/com/capgemini/springmvcproducts/beans/ProductInfoBean.java
@@ -0,0 +1,53 @@
+package com.capgemini.springmvcproducts.beans;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "product_info")
+public class ProductInfoBean implements Serializable {
+
+	@Id
+	@Column
+	private int productId;
+	@Column
+	private String productName;
+	@Column
+	private double productPrice;
+	@Column
+	private String productDescription;
+	
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	
+	
+}
